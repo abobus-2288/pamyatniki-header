85a898d663864f3dec067f902301b643457df224
Eugene Lobach <ucta456@gmail.com>
2022-05-11 21:46:33 +0300

    4

--- header.js
+++ header.js
@@ -1,28 +1,34 @@
 const test = () => {
   const button = document.querySelector('.menu');
+  const menu = document.querySelector('.main');
   
-  let state;
+  let buttonState;
+  let menuState;
   
   if (button.classList.contains('menu_opened'))
   {
-    state = 'opened';
+    buttonState = 'opened';
+    menuState = buttonState;
   }
   
   if (button.classList.contains('menu_closed'))
   {
-    state = 'closed';
+    buttonState = 'closed';
+    menuState = buttonState;
   }
   
-  switch (state) {
+  switch (buttonState) {
     case 'closed': {
-      console.log('Closed')
-      button.classList.replace('menu_closed', 'menu_opened')
+      // console.log('Closed');
+      button.classList.replace('menu_closed', 'menu_opened');
+      menu.classList.replace('main_closed', 'main_opened');
       break;
     }
     
     case 'opened': {
-      console.log('Opened');
-      button.classList.replace('menu_opened', 'menu_closed')
+      // console.log('Opened');
+      button.classList.replace('menu_opened', 'menu_closed');
+      menu.classList.replace('main_opened', 'main_closed');
       break;
     }
   }
